7ab6d7f7022e6b67da9cef8647c30b40f70fc893
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-25 00:49:40 -0400

    more keybinding config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ extern crate penrose;
 
 use penrose::{
     core::{
-        bindings::MouseEvent, config::Config, helpers::index_selectors, manager::WindowManager,
+        bindings::MouseEvent,
+        config::Config,
+        helpers::index_selectors,
+        layout::{side_stack, Layout, LayoutConf},
+        manager::WindowManager,
     },
     logging_error_handler,
     xcb::new_xcb_backed_window_manager,
@@ -11,35 +15,51 @@ use penrose::{
 };
 
 fn main() -> penrose::Result<()> {
-    let config = Config::default();
     let hooks = vec![];
 
     let key_bindings = gen_keybindings! {
+        // basics
         "A-j" => run_internal!(cycle_client, Forward);
         "A-k" => run_internal!(cycle_client, Backward);
         "A-S-j" => run_internal!(drag_client, Forward);
         "A-S-k" => run_internal!(drag_client, Backward);
         "A-S-c" => run_internal!(kill_client);
-        "A-Tab" => run_internal!(toggle_workspace);
-        // "M-bracketright" => run_internal!(cycle_screen, Forward);
-        // "M-bracketleft" => run_internal!(cycle_screen, Backward);
-        // "M-S-bracketright" => run_internal!(drag_workspace, Forward);
-        // "M-S-bracketleft" => run_internal!(drag_workspace, Backward);
-        // "M-grave" => run_internal!(cycle_layout, Forward);
-        // "M-S-grave" => run_internal!(cycle_layout, Backward);
-        // "M-A-Up" => run_internal!(update_max_main, More);
-        // "M-A-Down" => run_internal!(update_max_main, Less);
-        // "M-A-Right" => run_internal!(update_main_ratio, More);
-        // "M-A-Left" => run_internal!(update_main_ratio, Less);
+        "A-H" => run_internal!(update_main_ratio, Less);
+        "A-L" => run_internal!(update_main_ratio, More);
+        "A-P" => run_internal!(cycle_layout, Forward);
+        "A-I" => run_internal!(update_max_main, More);
+        "A-D" => run_internal!(update_max_main, Less);
         "A-S-q" => run_internal!(exit);
-        "A-Return" => run_external!("st");
+        "A-S-Return" => run_external!("st");
 
+        // screens
+        "M-period" => run_internal!(cycle_screen, Forward);
+        "M-comma" => run_internal!(cycle_screen, Backward);
+        "M-S-period" => run_internal!(drag_workspace, Forward);
+        "M-S-comma" => run_internal!(drag_workspace, Backward);
+
+        // workspace
+        "A-Tab" => run_internal!(toggle_workspace);
         map: { "1", "2", "3", "4", "5", "6", "7", "8", "9" } to index_selectors(9) => {
             "A-{}" => focus_workspace (REF);
             "A-S-{}" => client_to_workspace (REF);
         };
     };
 
+    let mut config_builder = Config::default().builder();
+    config_builder.workspaces(vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+
+    config_builder.layouts(vec![
+        Layout::new("[side]", LayoutConf::default(), side_stack, 1, 0.5),
+        Layout::floating("[-]"),
+    ]);
+
+    // colors
+    config_builder
+        .focused_border("#cc241d")?
+        .unfocused_border("#3c3836")?;
+
+    let config = config_builder.build().unwrap();
     let mut wm = new_xcb_backed_window_manager(config, hooks, logging_error_handler())?;
     wm.grab_keys_and_run(key_bindings, map! {})?;
 
